6c6730bd214a742a882cf26d88b745e6893a8a4e
MvStels <visotskiymaks1@gmail.com>
2024-01-15 21:29:35 +0200

    [hw_19]Roles_And_Permissions

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            #name VARCHAR(35)
             $table->string('name',35);
             $table->string('surname',100);
             $table->string('email')->unique();
