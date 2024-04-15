619b44435c1b0246df1b770f4d75536b98f1998b
MvStels <visotskiymaks1@gmail.com>
2024-04-15 16:20:52 +0300

    boot

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -15,27 +15,27 @@ import $ from 'jquery'
 window.$ = $;
 
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-import Echo from 'laravel-echo';
-
-import Pusher from 'pusher-js';
-window.Pusher = Pusher;
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    forceTLS: false,
-});
-Pusher.logToConsole = true;
-
-console.log('window.userID', window.userID)
-window.Echo.private(`App.Models.User.${window.userID}`)
-    .listen('UserNotify', (e) => {
-        alert(e.message)
-    })
+// /**
+//  * Echo exposes an expressive API for subscribing to channels and listening
+//  * for events that are broadcast by Laravel. Echo and event broadcasting
+//  * allows your team to easily build robust real-time web applications.
+//  */
+//
+// import Echo from 'laravel-echo';
+//
+// import Pusher from 'pusher-js';
+// window.Pusher = Pusher;
+//
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+//     forceTLS: false,
+// });
+// Pusher.logToConsole = true;
+//
+// console.log('window.userID', window.userID)
+// window.Echo.private(`App.Models.User.${window.userID}`)
+//     .listen('UserNotify', (e) => {
+//         alert(e.message)
+//     })
